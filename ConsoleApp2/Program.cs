92385aca48414cf2afb47c42922231851dd44f01
uczen <uczen@A26dell_stol11>
2023-02-06 14:54:02 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Algorytm Euklidesa
+        /// </summary>
+        /// <param name="a">Pierwsza liczba</param>
+        /// <param name="b">Druga liczba</param>
+        /// <returns>Najwyższy wspólny dzielnik</returns>
+        static int NWD(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            try
+            {
+                Console.WriteLine("Podaj a");
+                a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj b");
+                b = int.Parse(Console.ReadLine());
+
+                int wynik = NWD(a, b);
+                Console.WriteLine("Wynik: " + wynik);
+            }
+            catch (FormatException Ex)
+            {
+                Console.WriteLine("Podano zmienną w złym formacie");
+            }
+        }
+    }
+}
